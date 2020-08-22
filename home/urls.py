40b8c47d4ca4067cb5f714d43ea370d305245f87
hepepsolution <hepepsolutions@gmail.com>
2020-08-22 02:20:48 +0100

    I have added urls, views and templates for the about us, contact us, our services, service detail, service booking, rent a car.

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('testimonial/', views.testimonial, name='testimonial'),
     path('blog/', views.blog, name='blog'),
     path('blog_detail/', views.blog_detail, name='blog_detail'),
+    path('about-us/', views.about_us, name='about-us'),
+    path('contact/', views.contact, name='contact'),
+    path('service-details/', views.service_details, name='service-details'),
+    path('services/', views.services, name='services'),
+    path('rent-car/', views.rent_car, name='rent-car'),
 ]
--- home/views.py
+++ home/views.py
@@ -34,3 +34,22 @@ def blog_detail(request):
     template_name = 'home/blog-detail.html'
     return render(request, template_name, {})
 
+def about_us(request):
+    template_name = 'home/about-us.html'
+    return render(request, template_name, {})
+
+def contact(request):
+    template_name = 'home/contact.html'
+    return render(request, template_name, {})
+
+def services(request):
+    template_name = 'home/service.html'
+    return render(request, template_name, {})
+
+def service_details(request):
+    template_name = 'home/service-details.html'
+    return render(request, template_name, {})
+
+def rent_car(request):
+    template_name = 'home/rent-car.html'
+    return render(request, template_name, {})
